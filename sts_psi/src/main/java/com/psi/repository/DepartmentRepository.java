09951cbcb6dfde7553e30e484e0fee733b34eb0d
user <user@LAPTOP-R3VCBJ18>
2022-04-12 00:01:28 +0800

    psi

---
+++ sts_psi/src/main/java/com/psi/repository/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.psi.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.psi.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long>{
+	
+}
